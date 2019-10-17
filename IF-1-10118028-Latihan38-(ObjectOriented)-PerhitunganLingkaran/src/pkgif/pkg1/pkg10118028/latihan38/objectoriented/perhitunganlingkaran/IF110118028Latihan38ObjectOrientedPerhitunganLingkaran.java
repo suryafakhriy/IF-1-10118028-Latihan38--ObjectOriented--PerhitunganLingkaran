05d373766121a59bb352567593ae3a349a95b346
suryafakhriy <55834014+suryafakhriy@users.noreply.github.com>
2019-10-17 23:24:00 +0700

    Add files via upload

---
+++ IF-1-10118028-Latihan38-(ObjectOriented)-PerhitunganLingkaran/src/pkgif/pkg1/pkg10118028/latihan38/objectoriented/perhitunganlingkaran/IF110118028Latihan38ObjectOrientedPerhitunganLingkaran.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkgif.pkg1.pkg10118028.latihan38.objectoriented.perhitunganlingkaran;
+
+/**
+ *
+ * @author User
+ * NAMA      : Surya Fakhriy Hasbi
+   KELAS     : IF-01 2018
+   NIM       : 10118028
+   DESKRIPSI : (Object Oriented) Perhitungan Lingkaran
+
+ */
+public class IF110118028Latihan38ObjectOrientedPerhitunganLingkaran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Lingkaran ling = new Lingkaran();
+
+        System.out.println("=======Perhitungan Lingkaran=======");
+        ling.validasiInput();
+        ling.hasilPerhitungan(ling.diameter);
+    }
+    
+}
